62de163563086db94bdb60ed243c6f505cff0461
Michael <331383616@qq.com>
2019-11-02 18:23:40 +0800

    reactHooks201911-2_02

--- src/Example8.js
+++ src/Example8.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef ,useEffect,useState} from 'react';
 
 function Example8(){
     const inputEl = useRef(null)
@@ -7,10 +7,28 @@ function Example8(){
        console.log(inputEl)
     }
 
+    const [text, setText] = useState('jspang')
+    const textRef = useRef()
+
+    useEffect(()=>{
+        textRef.current =text;
+        console.log('textRef.current:', textRef.current)
+    })
+
     return (
         <div>
             <input ref={inputEl} type="text" />
             <button onClick={onButtonClick}>在input 上展示文字</button>
+
+            <br />
+            <hr />
+            <br />
+            <hr />
+
+
+            <input value={text} onChange={(e)=>{setText(e.target.value)}} />
+
+
 
         </div>
     )
